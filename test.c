e55babdbdf81a09b112a621e88945c33224b97d0
Srikanth Anantharam <sria91@gmail.com>
2018-02-13 22:29:29 +0530

    commit

--- test.c
+++ test.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <errno.h>
+#include <assert.h>
 
 int main() {
     printf("EDOM %d\n", EDOM);
     printf("EILSEQ %d\n", EILSEQ);
     printf("ERANGE %d\n", ERANGE);
+    assert(1 == 0);
 }
